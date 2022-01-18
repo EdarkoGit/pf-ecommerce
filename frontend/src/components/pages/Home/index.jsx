080188339a01b132425b6533caf2141eeff00231
Adrian Garcia <garciasaaib@gmail.com>
2022-01-17 20:46:48 -0700

    creando componentes Home y Landing

---
+++ frontend/src/components/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div>Home should have (i guess)
+      <ul>
+        <li>header</li>
+        <li>filters</li>
+        <li>lista de productos</li>
+        <li>footer</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
---
+++ frontend/src/components/pages/Landing/index.jsx
@@ -0,0 +1,13 @@
+const Landing = () => {
+  return (
+    <div>Landing should have (i guess)
+      <ul>
+        <li>Great cover image</li>
+        <li>Button to enter</li>
+        <li>Super enganchable copypaste text</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Landing;
